2e97a6354809589a7340a9ed5b94b789353383ed
rulessbroker <rulessbroker>
2023-01-20 22:52:22 +0530

    Modified:  use service layer to get message

---
+++ src/main/java/com/bridgelabz/greetingapp/controller/MessageController.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.greetingapp.controller;
+
+import com.bridgelabz.greetingapp.model.*;
+import com.bridgelabz.greetingapp.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/home")
+public class MessageController {
+	@Autowired
+	MessageService service;
+
+	@GetMapping("/message")
+	public String getData() {
+		return "<h1><font color=blue style=bold>Welcome to my Message App!</font></h1>";
+	}
+
+	@GetMapping("/get")
+	public String message(@RequestParam(value = "nane", defaultValue = "Sneha") String name) {
+		return "<h1><font color=red>Hii " + name + " ! Welcome to BridgeLabz!</font></h1>";
+	}
+
+	@PostMapping("/post")
+	public String post(@RequestBody Entity entity) {
+		String response = service.getService(entity);
+		return "<h1><font style=bold> Your full name is " + response + "</font></h1>";
+	}
+
+	@PutMapping("/put/{firstName}")
+	public String put(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!";
+	}
+
+	@GetMapping("/greet/{name}")
+	public String greet(@PathVariable String name) {
+		String given = service.getGreet(name);
+		return given;
+	}
+}
--- src/main/java/com/bridgelabz/greetingapp/service/MessageService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/MessageService.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
 	public String getService(Entity entity) {
-	     String response=entity.getFirstName()+" "+entity.getLastName();
-	     return response;
-	    }
+		String response = entity.getFirstName() + " " + entity.getLastName();
+		return response;
+	}
+
+	public String getGreet(String name) {
+		return "<h1><font color=pink>Hello World " + name + " !</font></h1>";
+	}
 }
